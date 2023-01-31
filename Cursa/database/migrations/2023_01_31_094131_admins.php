5f645040d92c35f59b4af31d4ec85b0723c8cfdc
civic100 <114212306+civic100@users.noreply.github.com>
2023-01-31 13:23:00 +0100

    db

--- Cursa/database/migrations/2023_01_31_094131_admins.php
+++ Cursa/database/migrations/2023_01_31_094131_admins.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->string('nome');
+            $table->string('email');
             $table->string('password');
             $table->timestamps();
         });
