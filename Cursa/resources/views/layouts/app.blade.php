f9e178386146426828d76ad20ba39be7433ac44e
davidFeid <davidbuny@gmail.com>
2023-03-10 10:57:33 +0100

    menu update

--- Cursa/resources/views/layouts/app.blade.php
+++ Cursa/resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 </head>
 <body>
     <div id="app">
-        @include('layouts.headerGeneral');
+        @include('layouts.headerGeneral')
         <main class="py-4">
             @yield('content')
         </main>
